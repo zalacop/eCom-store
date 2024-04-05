7dd5084ede930e3436aa869cc630bd3f2ef90e0a
zalacop <103320914+zalacop@users.noreply.github.com>
2024-04-05 23:34:30 +0200

    adds number next to the cart icon

--- src/components/CartIcon/index.jsx
+++ src/components/CartIcon/index.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import { MdOutlineShoppingBag } from "react-icons/md";
 import { useNavigate } from "react-router-dom";
+import useProductsStore from "../store/products";
 
 function CartIcon() {
+    const { cart } = useProductsStore();
+    const productCount = cart.reduce((total, product) => total + product.quantity, 0)
+
+
     const navigate = useNavigate();
 
     function handleCartClick() {
@@ -12,6 +17,11 @@ function CartIcon() {
     return (
         <div onClick={handleCartClick}>
             <MdOutlineShoppingBag />
+            {productCount > 0 && ( 
+                <div>
+                    {productCount}
+                </div>
+            )}
         </div>
     );
 }
