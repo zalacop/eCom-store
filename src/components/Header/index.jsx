5f4213f5206e088e2cb8eb2e04ef4a4c054d7d5c
zalacop <103320914+zalacop@users.noreply.github.com>
2024-04-07 14:40:03 +0200

    updates header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,25 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import CartIcon from "../CartIcon";
 import Nav from "../Nav";
 import { Link } from "react-router-dom";
 
 function Header() {
+
+    const [isOpen, setIsOpen] = useState(false);
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
     return (
-        <header className="sticky-top m-0">
-        <div className="container">
-            <div className="d-flex justify-content-start align-items-center gap-5">
-                <div className="col-md-4">
-                    <Nav />
+        <header className="sticky-top m-0 w-100">
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container">
+                    <button
+                        className="navbar-toggler border-0"
+                        type="button"
+                        onClick={toggleMenu}
+                    >
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <Link to="/" className="navbar-brand d-flex justify-content-center">
+                        <h1 className="fs-2">eCom Store</h1>
+                    </Link>
+                    <div className="d-lg-none ml-auto">
+                        <CartIcon />
+                    </div>
+                    <div className={"collapse navbar-collapse" + (isOpen ? " show" : "")}>
+                        <ul className="navbar-nav ml-auto">
+                            <li className="nav-item">
+                                <Link to="/" className="nav-link" onClick={toggleMenu}>Home</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to="/contact" className="nav-link" onClick={toggleMenu}>Contact Us</Link>
+                            </li>
+                        </ul>
+                        <div className="d-none d-lg-block">
+                            <CartIcon />
+                        </div>
+                    </div>
                 </div>
-                <Link to="/" className="col-md-4 text-center">
-                    <h1 className="fs-2">eCom Store</h1>
-                </Link>
-                <div className="col-md-4 text-right">
-                    <CartIcon />
-                </div>
-            </div>
-        </div>
-    </header>
+            </nav>
+        </header>
     );
 }
 
